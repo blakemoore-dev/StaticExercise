7cb763975d794247a20c5c746606ac8dc9bf8abc
blakemoore-dev <lostinthestack@protonmail.com>
2021-10-12 11:51:56 -0500

    init

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Celcius:    {TempConverter.FahrenheitToCelcius(12)}");
+            Console.WriteLine($"Celcius:    {TempConverter.CelciusToFahrenheit(-11.11)}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double f)
+        {
+            return (f - 32) / 1.8;
+        }
+        public static double CelciusToFahrenheit(double c)
+        {
+            return (c * 1.8) + 32;
+        }
+    }
+}
